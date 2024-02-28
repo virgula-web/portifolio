44d4388759ae8e21952d50e7fc3315168a7ece2f
Carlos Grein <carlosgrein102@gmail.com>
2024-02-28 16:09:35 -0300

    Merge branch 'main' of https://github.com/virgula-web/portifolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de JavaScript para interação com a página
+document.addEventListener('DOMContentLoaded', function() {
+    // Exemplo de alerta ao carregar a página
+    alert('Bem-vindo à página ao meu portfólio!');
+});
